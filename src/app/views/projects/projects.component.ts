fd6994c9de4afea5b9ad877793c6563d8c8b60b0
hina <hinaimranbutt97@gmail.com>
2022-10-02 18:37:46 +0500

    project page initial setup

--- src/app/views/projects/projects.component.ts
+++ src/app/views/projects/projects.component.ts
@@ -9,27 +9,43 @@ export class ProjectsComponent implements OnInit {
 
   constructor() { }
 
-  array1=[
-    {title:'Power Plants/ Oil & Gas', img:'../../../assets/images/closeup-view-cylindrical-grinder-industrial-concept.jpg'},
-    {title: 'Business & Commercial Buildings', img:'../../../assets/images/closeup-view-cylindrical-grinder-industrial-concept.jpg'},
-    {title:'Manufacturing Setup', img:'../../../assets/images/closeup-view-cylindrical-grinder-industrial-concept.jpg'},
-    {title:'Data Centers', img:'../../../assets/images/closeup-view-cylindrical-grinder-industrial-concept.jpg'},
-    {title:'Food & Beverages', img:'../../../assets/images/closeup-view-cylindrical-grinder-industrial-concept.jpg'},
-    {title:'Hospitals', img:'../../../assets/images/closeup-view-cylindrical-grinder-industrial-concept.jpg'},
-    {title:'Pharmaceuticals', img:'../../../assets/images/closeup-view-cylindrical-grinder-industrial-concept.jpg'},
-    {title:'Educational Buildings', img:'../../../assets/images/closeup-view-cylindrical-grinder-industrial-concept.jpg'}
+  array1 = [
+    { title: 'Power Plants/ Oil & Gas', img: '../../../assets/images/closeup-view-cylindrical-grinder-industrial-concept.jpg' },
+    { title: 'Business & Commercial Buildings', img: '../../../assets/images/closeup-view-cylindrical-grinder-industrial-concept.jpg' },
+    { title: 'Manufacturing Setup', img: '../../../assets/images/closeup-view-cylindrical-grinder-industrial-concept.jpg' },
+    { title: 'Data Centers', img: '../../../assets/images/closeup-view-cylindrical-grinder-industrial-concept.jpg' },
+    { title: 'Food & Beverages', img: '../../../assets/images/closeup-view-cylindrical-grinder-industrial-concept.jpg' },
+    { title: 'Hospitals', img: '../../../assets/images/closeup-view-cylindrical-grinder-industrial-concept.jpg' },
+    { title: 'Pharmaceuticals', img: '../../../assets/images/closeup-view-cylindrical-grinder-industrial-concept.jpg' },
+    { title: 'Educational Buildings', img: '../../../assets/images/closeup-view-cylindrical-grinder-industrial-concept.jpg' }
   ]
-  
-powerPlants=[
-  'MAN ATLAS POWER PLANT (211 MW) ',
-  'BALLOKI / ORIENT POWER PLANT (225 MW) ',
-  'SAPPHIRE POWER PLANT / MURIDKE (225 MW)' ,
-  'HUBCO POWER PLANT (211 MW)' ,
-  'MAKORI OIL FIELD ',
-  'SAWAN GAS FIELD (2700 TR) ',
- 'PARCO OIL FIELD (1000 TR) '
-  
-]
+
+  powerPlants = [
+    'MAN ATLAS POWER PLANT (211 MW) ',
+    'BALLOKI / ORIENT POWER PLANT (225 MW) ',
+    'SAPPHIRE POWER PLANT / MURIDKE (225 MW)',
+    'HUBCO POWER PLANT (211 MW)',
+    'MAKORI OIL FIELD ',
+    'SAWAN GAS FIELD (2700 TR) ',
+    'PARCO OIL FIELD (1000 TR) '
+  ]
+
+  business = [
+    'HYPER STAR LAHORE (1350 TR) ',
+    'ICI Morinaga Plant Sheikhupura ',
+    'TRICON CORPORATE TOWER LAHORE (2400 TR) ',
+    'DOLMEN CITY HYPER STAR (600 TR) ',
+    'NISHAT BOTIQUE HOTEL LAHORE (200 TR) ',
+    'BANK ALFALAH HEAD OFFICE KARACHI (1100 TR)',
+    'CHENONE TOWER MULTAN',
+    'PARKLANE TOWER LAHORE',
+    'HYPERSTAR WORLD TRADE CENTER ISLAMABAD',
+    'ISLAMABAD CLUB',
+    'MCB LAHORE'
+  ]
+
+
+
 
 
 
